e34933300e28ef2d52d49bdde77605f7338d8567
Vittor Fonseca Serra <vittorserradev@gmail.com>
2023-08-14 11:27:21 -0300

    Adicionei a class pointer-events-none

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,12 @@
 import { useEffect, useState } from "react";
 import "../Home.css";
-import api from "../services/api";
 
 export default function Home(){
 
-const [print,setPrint]= useState('');
+const [count,setCount] = useState(0);
 useEffect(()=>{
-    api.get("/register/product/new").then(res=>{
-        const titulo = res.data[0].titulo;
-        setPrint(titulo);
-    });
-},[]);
+    document.title = `Números de Cliques ${count}`
+})
 
 const [Foto] = useState(["https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg","https://images.pexels.com/photos/4169899/pexels-photo-4169899.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1","https://images.pexels.com/photos/7389105/pexels-photo-7389105.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1","https://images.pexels.com/photos/2657669/pexels-photo-2657669.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1","https://images.pexels.com/photos/15422831/pexels-photo-15422831/free-photo-of-arvore-de-natal-computador-portatil-laptop-notebook.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1","https://images.pexels.com/photos/8278851/pexels-photo-8278851.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1","https://images.pexels.com/photos/5882698/pexels-photo-5882698.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1","https://images.pexels.com/photos/17615043/pexels-photo-17615043/free-photo-of-mesa-balcao-escritorio-em-casa-home-office.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1","https://images.pexels.com/photos/5196814/pexels-photo-5196814.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1","https://images.pexels.com/photos/8297162/pexels-photo-8297162.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1","https://images.pexels.com/photos/7584231/pexels-photo-7584231.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1","https://images.pexels.com/photos/5702275/pexels-photo-5702275.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1","https://images.pexels.com/photos/7314827/pexels-photo-7314827.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" ,"https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg","https://images.pexels.com/photos/3571943/pexels-photo-3571943.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1","https://images.pexels.com/photos/14585225/pexels-photo-14585225.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1","https://images.pexels.com/photos/12290556/pexels-photo-12290556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1","https://images.pexels.com/photos/845245/pexels-photo-845245.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1","https://images.pexels.com/photos/2839120/pexels-photo-2839120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1","https://images.pexels.com/photos/274973/pexels-photo-274973.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1","https://images.pexels.com/photos/2928175/pexels-photo-2928175.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1","https://images.pexels.com/photos/1983040/pexels-photo-1983040.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"])
 
@@ -30,7 +26,7 @@ const [Foto] = useState(["https://images.pexels.com/photos/90946/pexels-photo-90
                         <div className="mb-2.5 w-full break-inside-avoid hover:cursor-pointer" key={index}>
                             <img
                                 loading="lazy"
-                                className="max-w-full rounded-xl"
+                                className="max-w-full rounded-xl pointer-events-none"
                                 src={link}
                                 alt={`Imagem ${index + 1}`}
                             />
@@ -39,7 +35,7 @@ const [Foto] = useState(["https://images.pexels.com/photos/90946/pexels-photo-90
                 </div>
             </section>
             <section className="h-[400px] flex items-center justify-center p-3 bg-black text-white border-b-2 border-white">
-                <h1 className="text-4xl font-bold">{print}</h1>
+                <button onClick={()=>setCount(count+1)}>Clique Aqui</button>
             </section>
         </main>
     )
